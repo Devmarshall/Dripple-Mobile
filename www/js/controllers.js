3d0dbf1d390a2cc7491c0a09147ae8ffb11bab82
somto <somto.achu5@gmail.com>
2016-12-30 15:52:14 +0100

    done with items

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,25 +2,58 @@ angular.module('starter.controllers', [])
 .controller('itemctrl',function($scope){
 
 
+
+
+
+  $scope.options = {
+    loop: false,
+    effect: 'fade',
+    speed: 500,
+  }
+
+  $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+    // data.slider is the instance of Swiper
+    $scope.slider = data.slider;
+  });
+
+  $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+    console.log('Slide change is beginning');
+  });
+
+  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    // note: the indexes are 0-based
+    $scope.activeIndex = data.slider.activeIndex;
+    $scope.previousIndex = data.slider.previousIndex;
+  });
+
+
+
+
+
+  $scope.d=function(){
+    console.log("description");
+    $scope.view={
+      comments:false,
+      d:true,
+      // user:true/////////for now  true
+    }
+  }
+
 $scope.view={
   comments:false,
   d:true,
   user:true/////////for now  true
 }
-$scope.change=function(){
-if($scope.view.comments===false){
+$scope.comment=function(){
+  console.log("comment");
   $scope.view={
     comments:true,
-    d:false
+    d:false,
+    // user:true/////////for now  true
   }
 }
-else{
-  $scope.view={
-    comments:false,
-    d:true
-    }
-}
-}
+
+
 })
 
 
